0edd55d1bebab42e69395f3fa8f9eca2486473eb
varshum12 <varshum12@gmail.com>
2025-05-15 05:12:23 +0000

    123

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-import strea
+import streamlit as st
+from client import StockApi
+
+
+# Create page title 
+
+st.set_page_config("Stock Market app")
+
+
+# add title
+
+st.title("Stock Market app")
+
+
+# add subheading 
+
+st.subheader("By Varsha Mhetre")
+
+
